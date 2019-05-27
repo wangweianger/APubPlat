97e7091b23bf5ca100602d3d72423497d207414c
wangweianger <752636052@qq.com>
2019-05-27 16:28:16 +0800

    add login

--- config/config.prod.js
+++ config/config.prod.js
@@ -6,7 +6,7 @@ module.exports = () => {
     config.debug = false;
 
     // 用于安全校验和回调域名根路径 生产线域名（必填）
-    config.origin = 'https://pc.juxingyi.com';
+    config.origin = 'https://test-apub.juxingyi.com';
 
     return config;
 };
